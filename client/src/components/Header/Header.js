61e2b80fe4eaa77181ba21a2cc807cbab6373d09
mo-aden <mohamedaden17@outlook.com>
2023-01-18 14:12:07 -0700

    modified the components/Header folder and updated all the dependencies required

---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./header.css";
+
+function Header() {
+  return (
+    <div className="header">
+      <h2>Logo</h2>
+      <ul>
+        <li>Sign Up !!</li>
+        <li>Login !!</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Header;
