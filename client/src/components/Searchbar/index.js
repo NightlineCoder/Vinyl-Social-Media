43c33b4fb85b0f4cb055114d82e7e689abeec469
rsheridan613 <rsheridan613@gmail.com>
2023-01-19 09:49:40 -0700

    added more note + menu components. finished feed, form, recent, searchbar components

---
+++ client/src/components/Searchbar/index.js
@@ -0,0 +1,9 @@
+function SearchBar() {
+  <div>
+    <form className="custom-searchbar">
+      <input id="search" type="text" placeholder="Search" />
+    </form>
+  </div>;
+}
+
+export default SearchBar;
