1a31d24a36bf6f9309e771d47abf02b767e774a5
mo-aden <mohamedaden17@outlook.com>
2023-01-22 20:18:27 -0700

    created the header component to be used by drew/frontend

---
+++ client/src/components/Homepage/Homepage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./homepage.css";
+
+function Homepage() {
+  return (
+    <div className="homepage">
+      <h2>Welcome to Vinyl Social Media App</h2>
+    </div>
+  );
+}
+
+export default Homepage;
