70a017b33d777bcbe4b11fc077825cde4d2fd0ef
rsheridan613 <rsheridan613@gmail.com>
2023-01-24 15:29:48 -0700

    cropped logo in assets

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -3,12 +3,14 @@ import React from "react";
 import { Typography, Box, Container, TextField } from "@mui/material";
 import Grid from "@mui/material/Unstable_Grid2";
 
+import logo from "../../assets/logo.cropped.png";
+
 const Nav = () => {
   return (
     <Grid container sx={{ backgroundColor: "primary.main", mt: 1 }}>
       <Grid xs={12}>
         <Typography variant="h3" component="h1" color="#F2F7F2">
-          "Logo will be here"
+          {logo}
         </Typography>
       </Grid>
     </Grid>
